f034ad9492d5c225225619a9ef5c2ef1cb78efe8
JessLo <jessicaklodge@gmail.com>
2018-01-03 13:29:04 +0000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "King";
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	console.log("Two cards have been played.");
+} else if (cardsInPlay[0] === (cardsInPlay[2]){
+	alert("You found a match!");
+}
+else { alert("Sorry, try again."); 
+}
